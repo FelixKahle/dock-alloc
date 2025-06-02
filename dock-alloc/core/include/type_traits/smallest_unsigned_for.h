65713e0c1e15046b1ce113b5d11de563587d3bdf
Felix Kahle <felix.kahle21@gmail.com>
2025-06-03 00:48:47 +0200

    Fix type in smallest_unsigned_for.h to use std::size_t instead of size_t

--- dock-alloc/core/include/type_traits/smallest_unsigned_for.h
+++ dock-alloc/core/include/type_traits/smallest_unsigned_for.h
@@ -28,7 +28,7 @@ namespace dockalloc::core
         using Type = std::conditional_t<N <= std::numeric_limits<uint8_t>::max(), uint8_t,
                         std::conditional_t<N <= std::numeric_limits<uint16_t>::max(), uint16_t,
                             std::conditional_t<N <= std::numeric_limits<uint32_t>::max(), uint32_t,
-                                std::conditional_t<(N <= std::numeric_limits<uint64_t>::max()), uint64_t, size_t>>>>;
+                                std::conditional_t<(N <= std::numeric_limits<uint64_t>::max()), uint64_t, std::size_t>>>>;
         // @formatter:on
     };
 
